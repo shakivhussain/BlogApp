22868a07846d94c95003055f3fdcb132dc4c4403
Shakiv Hussain <51313301+shakivhussain@users.noreply.github.com>
2023-05-02 01:13:36 +0530

    ✅ Remember Coroutine Scope | Side Effects

--- app/src/main/java/com/shakiv/husain/blogapp/MainActivity.kt
+++ app/src/main/java/com/shakiv/husain/blogapp/MainActivity.kt
@@ -1,45 +1,177 @@
 package com.shakiv.husain.blogapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.shakiv.husain.blogapp.ui.theme.BlogAppTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BlogAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+
+            BlogApp()
+
         }
     }
 }
 
+@Preview(showSystemUi = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BlogApp() {
+    CoroutineScopeComposable()
 }
 
-@Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
-    BlogAppTheme {
-        Greeting("Android")
+fun CoroutineScopeComposable() {
+
+    val counter = remember { mutableStateOf(0) }
+    var scope = rememberCoroutineScope()
+
+
+    var text = "Counter is running ${counter.value}"
+    if (counter.value == 10) {
+        text = "Counter Stopped."
     }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = text, style = MaterialTheme.typography.headlineSmall)
+
+        Button(onClick = {
+            scope.launch {
+
+                Log.d("TAG", "CoroutineScopeComposable: Started...")
+
+                try {
+                    for (i in 1..10) {
+                        Log.d("TAG", "CoroutineScopeComposable: $counter")
+                        counter.value++
+                        delay(1000)
+                    }
+                } catch (e: Exception) {
+                    Log.e("TAG", "Exception : ${e.message.toString()}:  ")
+                }
+            }
+        }) {
+
+            Text(text = "Start")
+
+        }
+
+    }
+
+}
+
+
+@Composable
+fun LaunchedEffectComposable() {
+
+    var counter = remember { mutableStateOf(0) }
+
+    LaunchedEffect(key1 = Unit) {
+        try {
+            for (i in 1..10) {
+                counter.value++
+                delay(1000)
+            }
+        } catch (e: Exception) {
+            Log.e("TAG", "Exception : ${e.message.toString()}:  ")
+        }
+
+    }
+
+    Button(onClick = {
+        counter.value++
+    }) {
+        Text(text = "Inc Count : ${counter.value}")
+    }
+}
+
+
+@Composable
+fun SideEffectExample() {
+
+
+    /*
+*  This function will side effect here because,
+*  fetching data inside the composable function
+*
+*/
+    val categoryState = remember { mutableStateOf(emptyList<String>()) }
+
+    // Will Run only one time
+    LaunchedEffect(key1 = Unit) {
+        categoryState.value = fetchData()
+    }
+
+    LazyColumn {
+
+        items(categoryState.value) {
+            Text(text = "$it")
+
+        }
+    }
+
+}
+
+fun fetchData(): List<String> {
+    return listOf<String>("One", "Two", "Three")
+}
+
+
+@Composable
+fun App() {
+
+    var theme = remember { mutableStateOf(false) }
+
+    // LocalContext.current.applicationContext
+
+    BlogAppTheme(theme.value) {
+        Column(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.background)
+        ) {
+
+            Text(text = "Shakiv Husain", style = MaterialTheme.typography.titleSmall)
+
+            Button(onClick = {
+                theme.value = !theme.value
+            }) {
+                Text(text = "Theme Change.")
+            }
+        }
+
+    }
+}
+
+
+var count: Int = 0
+
+@Composable
+fun HasSideEffect() {
+    count++
+    Text(text = "Count : $count")
 }
---
+++ app/src/main/java/com/shakiv/husain/blogapp/ui/theme/Shape.kt
@@ -0,0 +1,13 @@
+package com.shakiv.husain.blogapp.ui.theme
+
+import androidx.compose.material3.ShapeDefaults
+import androidx.compose.material3.Shapes
+
+val Shapes = Shapes(
+    // Shapes None and Full are omitted as None is a RectangleShape and Full is a CircleShape.
+    extraSmall = ShapeDefaults.ExtraSmall,
+    small = ShapeDefaults.Small,
+    medium = ShapeDefaults.Medium,
+    large = ShapeDefaults.Large,
+    extraLarge = ShapeDefaults.ExtraLarge,
+)
